bed3cb48db17bf6b363bac860c8f011d5c3915e9
Robin Smidsrød <robin@smidsrod.no>
2010-04-25 09:02:58 +0200

    First actual version of SectorChart that actually renders sectors

--- src/no/smidsrod/robin/svg/library/SectorChartRenderer.java
+++ src/no/smidsrod/robin/svg/library/SectorChartRenderer.java
@@ -1,8 +1,11 @@
 package no.smidsrod.robin.svg.library;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.io.OutputStream;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,13 +16,20 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 	private static final int VIEWBOX_WIDTH = 1000;
 	private static final int VIEWBOX_HEIGHT = 750;
 
+	private static final int HEADER_POSITION = 50;
+	private static final int HEADER_FONT_SIZE = 32;
+	private static final int HEADER_GUTTER = 24;
+
+	private static final int LEGEND_FONT_SIZE = 24;
+
+	private static final int SECTOR_MARGIN = 75;
+
 	private static final String SVG_NAMESPACE = "http://www.w3.org/2000/svg";
 	private static final String SVG_VERSION = "1.1";
 
 	private Chart chart;
 	private Document xmlDocument;
 	private boolean prettyPrint = false;
-	private double legendFontHeight = 24;
 
 	public SectorChartRenderer(Chart chart) {
 		if (chart == null) {
@@ -76,7 +86,7 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 	/**
 	 * The viewport is set so that we have a virtual canvas to paint on sized
 	 * according to VIEWBOX_WIDTH/HEIGHT which preserves aspect ratio.
-	 *
+	 * 
 	 * http://www.w3.org/TR/SVG/coords.html#ViewBoxAttribute
 	 * http://www.w3.org/TR/SVG/coords.html#PreserveAspectRatioAttribute
 	 */
@@ -133,9 +143,9 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 		// Create a text element for the title
 		Element text = xmlDocument.createElement("text");
 		text.setAttribute("x", "50%");
-		text.setAttribute("y", "50");
+		text.setAttribute("y", HEADER_POSITION + "");
 		text.setAttribute("font-family", "sans-serif");
-		text.setAttribute("font-size", "32");
+		text.setAttribute("font-size", HEADER_FONT_SIZE + "");
 		text.setAttribute("text-anchor", "middle");
 		text.setTextContent(chart.getTitle());
 		svg.appendChild(text);
@@ -211,20 +221,20 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 	}
 
 	private void drawLegendItem(int counter, Item item, Element g) {
-		double y = (counter + 1) * legendFontHeight + (legendFontHeight / 3);
+		double y = (counter + 1) * LEGEND_FONT_SIZE + (LEGEND_FONT_SIZE / 3);
 
 		Element circle = xmlDocument.createElement("circle");
 		circle.setAttribute("cx", "15");
 		circle.setAttribute("cy", (y - 9) + "");
-		circle.setAttribute("r", (legendFontHeight * 0.4) + "");
+		circle.setAttribute("r", (LEGEND_FONT_SIZE * 0.4) + "");
 		circle.setAttribute("fill", cssColor(item.getColor()));
 		g.appendChild(circle);
 
 		Element text = xmlDocument.createElement("text");
-		text.setAttribute("x", (legendFontHeight * 1.25) + "");
+		text.setAttribute("x", (LEGEND_FONT_SIZE * 1.25) + "");
 		text.setAttribute("y", y + "");
 		text.setAttribute("font-family", "sans-serif");
-		text.setAttribute("font-size", legendFontHeight + "");
+		text.setAttribute("font-size", LEGEND_FONT_SIZE + "");
 		text.setAttribute("stroke", "black");
 		text.setTextContent(item.getName());
 		g.appendChild(text);
@@ -240,7 +250,7 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 
 		// Use item count + 1 multiplied by font height
 		int itemCount = chart.getItemList().size();
-		double height = legendFontHeight * ((double) itemCount + 1);
+		double height = LEGEND_FONT_SIZE * ((double) itemCount + 1);
 
 		return height > maxHeight ? maxHeight : height;
 	}
@@ -259,9 +269,10 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 			}
 		}
 
-		// Sans-serif fonts have approx. 50% width compared to height (estimated)
-		double circleWidth = 15 + legendFontHeight * 0.4 * 2;
-		double width = legendFontHeight * 0.50 * maxLength + circleWidth + 10;
+		// Sans-serif fonts have approx. 50% width compared to height
+		// (estimated)
+		double circleWidth = 15 + LEGEND_FONT_SIZE * 0.4 * 2;
+		double width = LEGEND_FONT_SIZE * 0.50 * maxLength + circleWidth + 10;
 
 		return width > maxWidth ? maxWidth : width;
 	}
@@ -272,31 +283,141 @@ public class SectorChartRenderer extends AbstractSVGRenderer {
 		Element g = xmlDocument.createElement("g");
 		svg.appendChild(g);
 
+		Point center = calcCenter();
+		double radius = calcSectorRadius();
+
+		System.err.println("Center: " + center);
+
 		List<Item> items = chart.getItemList();
 		double sumValues = calcSumValues(items);
-		for(Item item: items) {
-			drawSectorItem(sumValues, item, g);
+		double startValue = 0;
+		for (Item item : items) {
+			double currentValue = getValueForItem(item);
+			double startPosition = calcPosition(startValue, sumValues);
+			double endPosition = calcPosition(startValue + currentValue,
+					sumValues);
+			drawSectorItem(center, radius, startPosition, endPosition, item, g);
+			startValue += currentValue;
 		}
+	}
+
+	private double calcSectorRadius() {
+		return (VIEWBOX_HEIGHT - calcHeaderHeight() - calcFooterHeight() - SECTOR_MARGIN * 2) / 2;
+	}
+
+	private int calcFooterHeight() {
+		// TODO Auto-generated method stub
+		return calcHeaderHeight();
+	}
+
+	private void drawSectorItem(Point center, double radius, double start,
+			double end, Item item, Element g) {
+		System.err.println(item.getName() + ": " + calcAngle(start) + " - "
+				+ calcAngle(end) + " (" + calcPercentage(start, end) + "%)");
+
+		String percentage = calcPercentage(start, end) + "%";
+		Element path = xmlDocument.createElement("path");
+		String pathData = calcPathData(center, start, end, radius, item
+				.isHighlighted());
+		path.setAttribute("d", pathData);
+		path.setAttribute("fill", cssColor(item.getColor()));
+		path.setAttribute("stroke", "black");
+		path.setAttribute("title", percentage);
+
+		g.appendChild(path);
+
+	}
+
+	private String calcPathData(Point center, double start, double end,
+			double radius, boolean isHighlighted) {
+
+		int offsetX = 0;
+		int offsetY = 0;
+
+		if (isHighlighted) {
+			double middle = (end - start) / 2 + start;
+			System.err.println("MiddleAngle:" + calcAngle(middle));
+			offsetX = calcXFromFraction(middle, center, radius * 0.5)
+					- center.x;
+			offsetY = calcYFromFraction(middle, center, radius * 0.5)
+					- center.y;
+		}
+
+		int centerX = center.x + offsetX;
+		int centerY = center.y + offsetY;
+
+		int startX = calcXFromFraction(start, center, radius) + offsetX;
+		int startY = calcYFromFraction(start, center, radius) + offsetY;
+
+		int endX = calcXFromFraction(end, center, radius) + offsetX;
+		int endY = calcYFromFraction(end, center, radius) + offsetY;
+
+		int xAxisOffset = 0; // calcAngle(start);
+
+		String moveToCenter = "M" + centerX + "," + centerY;
+		String lineToStart = " L" + startX + "," + startY;
+		String arcToEnd = " A" + radius + "," + radius + " " + xAxisOffset
+				+ " 0,1 " + endX + "," + endY;
+		// String lineToEnd = " L" + endX + "," + endY;
+		String closePath = " Z";
+
+		return moveToCenter + lineToStart + arcToEnd + closePath;
+	}
+
+	private int calcXFromFraction(double fraction, Point center, double radius) {
+		int angle = calcAngle(fraction);
+		// From http://math2.org/math/geometry/circles.htm
+		// x(t) = r cos(t) + j
+		return (int) (radius * Math.cos(Math.toRadians(angle)) + center.x);
+	}
+
+	private int calcYFromFraction(double fraction, Point center, double radius) {
+		int angle = calcAngle(fraction);
+		// From http://math2.org/math/geometry/circles.htm
+		// y(t) = r sin(t) + k
+		return (int) (radius * Math.sin(Math.toRadians(angle)) + center.y);
+	}
+
+	private double calcPosition(double value, double total) {
+		return value / total;
+	}
+
+	private double getValueForItem(Item item) {
+		if (item.getValueList().size() > 0) {
+			// Always use the first dimension of the first Value instance
+			return item.getValueList().get(0).get(chart.getRange(0));
+		}
+		throw new RuntimeException("There is no value for this item");
 	}
 
 	private double calcSumValues(List<Item> items) {
 		double sumValues = 0;
-		for(Item item: items) {
-			if ( item.getValueList().size() > 0 ) {
-				sumValues += item.getValueList().get(0).get(0);
-			}
+		for (Item item : items) {
+			sumValues += getValueForItem(item);
 		}
 		return sumValues;
 	}
 
-	private void drawSectorItem(double sumValues, Item item, Element g) {
-		double currentValue = 0;
-		if ( item.getValueList().size() > 0 ) {
-			currentValue = item.getValueList().get(0).get(0);
-		}
-		System.err.println(item.getName() + ": " + currentValue + " - "
-				+ sumValues + " (" + (currentValue / sumValues ) + ")");
-		// TODO
+	private Point calcCenter() {
+		int x = VIEWBOX_WIDTH / 2; // - (int) calcLegendWidth();
+		int y = VIEWBOX_HEIGHT / 2; // + calcHeaderHeight();
+		return new Point(x, y);
+	}
+
+	private int calcHeaderHeight() {
+		return HEADER_POSITION + HEADER_FONT_SIZE + HEADER_GUTTER;
+	}
+
+	private int calcAngle(double fraction) {
+		return (int) Math.round(fraction * 360);
+	}
+
+	private String calcPercentage(double start, double end) {
+		double fraction = Math.abs(start - end);
+		double percentage = fraction * 100;
+		NumberFormat nf = NumberFormat.getNumberInstance(Locale.ENGLISH);
+		nf.setMaximumFractionDigits(1);
+		return nf.format(percentage);
 	}
 
 }
